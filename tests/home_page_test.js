e3591c9b24b3c02540efd5ebd0e50aac8c222579
devs1993 <davidjuly93@gmail.com>
2018-08-05 21:20:57 +0530

    Update package.json
     * Fix failing test

--- tests/home_page_test.js
+++ tests/home_page_test.js
@@ -11,9 +11,7 @@ module.exports = {
         .assert.cssClassPresent('div#dev-div a:nth-of-type(3) i', 'fa-linkedin')
         .waitForElementVisible('div#about h2', browser.globals.constants.time_out)
         .waitForElementVisible('div#experience-timeline', browser.globals.constants.time_out)
-        // the next line will fail,
-        .verify.containsText('div#education div:nth-of-type(2) span', 'Date Range')
-        // as verify is used the failure is logged and execution continues
+        .assert.containsText('div#education div:nth-of-type(2) span', 'June 2011 - May 2014')
         .waitForElementVisible('div#projects .project-info', browser.globals.constants.time_out)
         .waitForElementVisible('div#skills', browser.globals.constants.time_out)
         .verify.containsText('div#skills li:nth-of-type(2)', 'Ruby on Rails')
